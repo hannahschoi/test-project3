13ce6dd7436e57b1ed029cad487f03e53505dd87
Hannah Choi <hannahs.choi@surreyschools.ca>
2022-11-14 11:47:09 -0800

    Commit Message

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -15,6 +15,8 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("HelloWorld1");
         // TODO code application logic here
     }
     
